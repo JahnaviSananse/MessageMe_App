80bc0ed675a12edd4ea41fd16c4c703f1bd984e5
JahnaviSananse <jahnavisananse@gmail.com>
2021-08-17 19:19:22 +0530

    Chatroom channel created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,43 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 require("semantic-ui-sass");
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+// Rails.start();
+// Turbolinks.start();
+// ActiveStorage.start();
+
+// $(document).on("turbolinks:load", function () {
+//   $(".ui.dropdown").dropdown();
+//   $(".message .close").on("click", function () {
+//     $(this).closest(".message").transition("fade");
+//   });
+//   if ($("#messages").length > 0) {
+//     $("#messages").scrollTop($("#messages")[0].scrollHeight);
+//   }
+//   $("#message_body").on("keydown", function (e) {
+//     if (e.keyCode == 13) {
+//       $("button").click();
+//       e.target.value = "";
+//     }
+//   });
+// });
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+  $("#message_body").on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+});
+
+// $(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
